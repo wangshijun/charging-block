ff3bbde646ecd4ee8c4c358368bc1a54d380ac43
polunzh <zhenqiang@arcblock.io>
2019-09-14 14:45:30 +0800

    add charging-poles api

--- api/routes/charging_pole.js
+++ api/routes/charging_pole.js
@@ -8,7 +8,24 @@ module.exports = {
   init(app) {
     // list charging pole
     app.get('/api/chargingPoles', async (req, res) => {
-      res.json({ chargingPoles: [] });
+      const dbPoles = await ChargingPole.find();
+      if (!Array.isArray(dbPoles) || dbPoles.length === 0) {
+        res.json({ chargingPoles: [] });
+      }
+
+      const size = 20;
+      const length = Math.floor(Math.random() * size) + 1;
+      const start = Math.floor(Math.random() * dbPoles.length);
+      const randomPoles = dbPoles.slice(start, start + length);
+      const poles = randomPoles
+        .map(pole => {
+          const tmp = pole.toObject();
+          tmp.distance = 100 + parseInt(Math.random() * 5 * 1000, 10);
+          return tmp;
+        })
+        .sort((x, y) => x.distance - y.distance);
+
+      res.json({ chargingPoles: poles });
     });
 
     // Register charging pole
