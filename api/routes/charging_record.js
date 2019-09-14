799c2195debf5a07827ec9a88d5901e4f79f573b
polunzh <zhenqiang@arcblock.io>
2019-09-14 18:29:21 +0800

    add charging records query and update api

--- api/routes/charging_record.js
+++ api/routes/charging_record.js
@@ -5,6 +5,42 @@ const ChargingPole = keystone.list('ChargingPole').model;
 
 module.exports = {
   init(app) {
+    app.get('/api/charging/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+        const record = await ChargingRecord.findById(id);
+        if (!record) {
+          return res.status(400).json(`Record ${id} not found`);
+        }
+
+        return res.json(record);
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'get charging failed' });
+      }
+    });
+
+    app.put('/api/charging/:id/disconnect', async (req, res) => {
+      try {
+        const { id } = req.params;
+        const record = await ChargingRecord.findById(id);
+        if (!record) {
+          return res.status(400).json(`Record ${id} not found`);
+        }
+
+        if (record.status === 'done') {
+          return res.json(record);
+        }
+
+        await record.update({ status: 'done', disconnectedAt: new Date() });
+        const result = await ChargingRecord.findById(record._id); // eslint-disable-line
+        return res.json(result);
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'disconnect charging failed' });
+      }
+    });
+
     app.post('/api/charging', async (req, res) => {
       try {
         const { carDid, chargingPoleDid } = req.body;
@@ -28,6 +64,7 @@ module.exports = {
         }
 
         const record = new ChargingRecord({
+          chargingPoleDid: chargingPole.did,
           chargingPole,
           status: 'charging',
           connectedAt: new Date(),
@@ -39,7 +76,7 @@ module.exports = {
         return res.json(record);
       } catch (err) {
         console.error('api.charging.error', err);
-        return res.json(null);
+        return res.status(500).json({ error: 'charging failed' });
       }
     });
   },
